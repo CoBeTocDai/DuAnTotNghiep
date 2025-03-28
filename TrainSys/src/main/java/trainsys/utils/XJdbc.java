3d514396260d95222e482877c68adaade6bc3183
Khanhhh-20007 <huynhseng40@gmail.com>
2025-03-28 16:09:39 +0700

    Giao Dien

---
+++ TrainSys/src/main/java/trainsys/utils/XJdbc.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trainsys.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Dien
+ */
+public class XJdbc {
+    private static String driver="com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    private static String dburl="jdbc:sqlserver://127.0.0.1:1433;databaseName=TrainSystem;encrypt=false;trustServerCertificate=true";
+    private static String username="dien";
+    private static String password="dien";
+    
+    static{
+        try {            
+            Class.forName(driver);
+            System.out.println("Driver loaded successfully!");
+        } 
+        catch (ClassNotFoundException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static PreparedStatement getStmt(String sql, Object...args) throws SQLException{
+        Connection connection = DriverManager.getConnection(dburl, username, password);
+        System.out.println("connection established: " + connection);
+        PreparedStatement pstmt = null;
+        if(sql.trim().startsWith("{")){
+            pstmt = connection.prepareCall(sql);
+        }
+        else{
+            pstmt = connection.prepareStatement(sql);
+        }
+        for(int i=0;i<args.length;i++){
+            pstmt.setObject(i + 1, args[i]);
+        }
+        return pstmt;
+    }
+
+    public static void update(String sql, Object...args) {
+        try {
+            PreparedStatement stmt = XJdbc.getStmt(sql, args);
+            try {
+                stmt.executeUpdate();
+                System.out.println("update executed");
+            } 
+            finally{
+                stmt.getConnection().close();
+                System.out.println("connection closed!");
+            }
+        } 
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static ResultSet query(String sql, Object...args) {
+        try {
+            PreparedStatement stmt = XJdbc.getStmt(sql, args);
+            return stmt.executeQuery();
+        } 
+        catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public static Object value(String sql, Object...args) {
+        try {
+            ResultSet rs = XJdbc.query(sql, args);
+            if(rs.next()){
+                return rs.getObject(0);
+            }
+            rs.getStatement().getConnection().close();
+            return null;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
