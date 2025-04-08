e2f32ecd08e09293b7beea21022067dcdda42c72
Khanhhh-20007 <huynhseng40@gmail.com>
2025-04-08 22:18:11 +0700

    ThongKe

--- TrainSys/src/main/java/trainsys/dao/ThongKeDAO.java
+++ TrainSys/src/main/java/trainsys/dao/ThongKeDAO.java
@@ -4,10 +4,38 @@
  */
 package trainsys.dao;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import trainsys.utils.XJdbc;
+
 /**
  *
  * @author Tien
  */
 public class ThongKeDAO {
+    private List<Object[]> getListOfArray(String sql, String[] cols, Object...args) {
+        try {
+            List<Object[]> list = new ArrayList<>();
+            ResultSet rs = XJdbc.query(sql, args);
+            while (rs.next()) {
+                Object[] vals = new Object[cols.length];
+                for (int i = 0; i < cols.length; i++) {
+                    vals[i] = rs.getObject(cols[i]);
+                }
+                list.add(vals);
+            }
+            rs.getStatement().getConnection().close();
+            return list;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public List<Object[]> getDoanhThu(int thang){
+        String sql = "{CALL sp_DoanhThu (?)}";
+        String[] cols = {"ChuyenDe", "SoKH", "SoHV",  "DoanhThu", "ThapNhat", "CaoNhat", "TrungBinh"};
+        return this.getListOfArray(sql, cols, thang);
+    }
     
 }
