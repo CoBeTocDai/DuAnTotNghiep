208ccd73244cd72afc2d07a81c743a2897f8743d
Khanhhh-20007 <huynhseng40@gmail.com>
2025-04-09 00:13:43 +0700

    TaoTK

--- TrainSys/src/main/java/trainsys/dao/TaoTKDAO.java
+++ TrainSys/src/main/java/trainsys/dao/TaoTKDAO.java
@@ -14,7 +14,7 @@ import java.sql.*;
  */
 public class TaoTKDAO {
     public void insert(TaoTK tk) {
-        String sql = "INSERT INTO KhachHang (MaKH, TenTaiKhoan, HoTen, SDT, Email, MatKhau) VALUES (?, ?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO TaoTK (MaKH, TenTaiKhoan, HoTen, SDT, Email, MatKhau) VALUES (?, ?, ?, ?, ?, ?)";
         XJdbc.update(sql,
                 tk.getMaKH(),
                 tk.getTenTaiKhoan(),
